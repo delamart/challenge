0effb9b81b2c4feab188cc359b51deb98b4746c1
Erik De Lamarter <erik@delamarter.ch>
2013-01-20 13:50:27 +0100

    Ignore config.ini and added example + boxfile db config

--- lib/db.class.php
+++ lib/db.class.php
@@ -12,9 +12,25 @@ class DbLib extends PDO {
     public static function getInstance($name = 'default') {
         
         if(!isset(self::$connections[$name])) {
-            $dsn = ConfigLib::g('database/dsn');
-            $username = ConfigLib::g('database/user');
-            $passwd = ConfigLib::g('database/passwd');            
+        
+        	switch(ConfigLib::g('database/from','config'))
+        	{
+        		case 'boxfile':
+	        		//mysql:dbname=challenge;host=127.0.0.1;port=3306
+        			$dsn = 	'mysql:dbname=' . $_SERVER["DB1_NAME"].
+        					';host='. $_SERVER["DB1_HOST"].
+        					';port='. $_SERVER["DB1_PORT"];
+        			$username = $_SERVER["DB1_USER"];        			                      
+					$passwd = $_SERVER["DB1_PASS"];
+					break;
+        		case 'config':
+        		default:
+            		$dsn = ConfigLib::g('database/dsn');
+            		$username = ConfigLib::g('database/user');
+            		$passwd = ConfigLib::g('database/passwd');
+            		break;
+            }
+            
             $cxn = new DbLib($dsn, $username, $passwd);
             $cxn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
             self::$connections[$name] = $cxn;
